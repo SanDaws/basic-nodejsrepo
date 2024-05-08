daf7d7485f296eb454043db42a9d6c1d5e3b3940
Daws <castrogiraldosantiago@gmail.com>
2024-05-08 13:02:29 -0500

    movimiento  database, ejercicio recorrido diferente

--- src/js/main.js
+++ src/js/main.js
@@ -3,62 +3,6 @@
 import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-import {coders} from './database.js'
-const tbody= document.querySelector('tbody')
-
-function impresionForof(params) {
-    for (const coder of coders) {
-        tbody.innerHTML+=`
-        <tr>
-            <th scope="row">${coder.id}</th>
-            <td>${coder.name}</td>
-            <td>${coder.lastName}</td>
-            <td>${coder.email}</td>
-        </tr>
-        `
-        
-    }
-}
-function impresionFor() {
-    for (let i = 0; i < coders.length; i++) {
-        const coder = coders[i];
-        tbody.innerHTML+=`
-        <tr>
-            <th scope="row">${coder.id}</th>
-            <td>${coder.name}</td>
-            <td>${coder.lastName}</td>
-            <td>${coder.email}</td>
-        </tr>
-        `
-        
-    }
-    
-}
-function impresionForEach() {
-    coders.forEach(coder=> {
-        tbody.innerHTML+=`
-        <tr>
-            <th scope="row">${coder.id}</th>
-            <td>${coder.name}</td>
-            <td>${coder.lastName}</td>
-            <td>${coder.email}</td>
-        </tr>
-        `
-    });
-    
-}
-function impresionForIn() {
-    for (const coder in coders) {
-        tbody.innerHTML+=`
-            <tr>
-                <th scope="row">${coders[coder].id}</th>
-                <td>${coders[coder].name}</td>
-                <td>${coders[coder].lastName}</td>
-                <td>${coders[coder].email}</td>
-            </tr>
-            `
-    }
-    
-    
-}
-impresionForIn()
+import {coders} from '../../public/data/database.js'
+import {impresionForof} from './operations.js'
+impresionForof(coders)
---
+++ src/js/operations.js
@@ -0,0 +1,59 @@
+const tbody= document.querySelector('tbody')
+
+export function impresionForof(coders) {
+    for (const coder of coders) {
+        tbody.innerHTML+=`
+        <tr>
+            <th scope="row">${coder.id}</th>
+            <td>${coder.name}</td>
+            <td>${coder.lastName}</td>
+            <td>${coder.email}</td>
+        </tr>
+        `
+        
+    }
+}
+function impresionFor() {
+    for (let i = 0; i < coders.length; i++) {
+        const coder = coders[i];
+        tbody.innerHTML+=`
+        <tr>
+            <th scope="row">${coder.id}</th>
+            <td>${coder.name}</td>
+            <td>${coder.lastName}</td>
+            <td>${coder.email}</td>
+        </tr>
+        `
+        
+    }
+    
+}
+function impresionForEach(coders) {
+    coders.forEach(coder=> {
+        tbody.innerHTML+=`
+        <tr>
+            <th scope="row">${coder.id}</th>
+            <td>${coder.name}</td>
+            <td>${coder.lastName}</td>
+            <td>${coder.email}</td>
+        </tr>
+        `
+    });
+    
+}
+function impresionForIn() {
+    for (const i in coders) {
+        const coder = coders[i];
+        
+        tbody.innerHTML+=`
+            <tr>
+                <th scope="row">${coder.id}</th>
+                <td>${coder.name}</td>
+                <td>${coder.lastName}</td>
+                <td>${coder.email}</td>
+            </tr>
+            `
+    }
+    
+    
+}

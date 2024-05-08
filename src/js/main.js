b641be8aa95f3f0518166c30be8009bcb392be9a
Daws <castrogiraldosantiago@gmail.com>
2024-05-08 12:37:24 -0500

    export e import a un archivo, impreson por metodos de for

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,50 @@
 //los estilos entran por javascript
 // Import our custom CSS
 import '../scss/styles.scss'
-
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import {coders} from './database.js'
+const tbody= document.querySelector('tbody')
+
+function impresionForof(params) {
+    for (const coder of coders) {
+        tbody.innerHTML+=`
+        <tr>
+            <th scope="row">${coder.id}</th>
+            <td>${coder.name}</td>
+            <td>${coder.lastName}</td>
+            <td>${coder.email}</td>
+        </tr>
+        `
+        
+    }
+}
+function impresionFor() {
+    for (let i = 0; i < coders.length; i++) {
+        const coder = coders[i];
+        tbody.innerHTML+=`
+        <tr>
+            <th scope="row">${coder.id}</th>
+            <td>${coder.name}</td>
+            <td>${coder.lastName}</td>
+            <td>${coder.email}</td>
+        </tr>
+        `
+        
+    }
+    
+}
+function impresionForEach() {
+    coders.forEach(coder=> {
+        tbody.innerHTML+=`
+        <tr>
+            <th scope="row">${coder.id}</th>
+            <td>${coder.name}</td>
+            <td>${coder.lastName}</td>
+            <td>${coder.email}</td>
+        </tr>
+        `
+    });
+    
+}
+impresionForEach()

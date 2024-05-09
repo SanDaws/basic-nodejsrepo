854a479f6cb93ebf0322b42ad1fcebe1731ec63a
Daws <castrogiraldosantiago@gmail.com>
2024-05-09 12:05:51 -0500

    submiting a formularie

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,17 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 import {coders} from '../../public/data/database.js'
 import {impresionForof} from './operations.js'
-impresionForof(coders)
+
+// impresionForof(coders,tbody)
+
+const fbody= document.querySelector('#fbody')
+
+const name = fbody.querySelector('input[name="name"]')
+const email = fbody.querySelector('input[name="mail"]')
+const lastName= fbody.querySelector('input[name="lastName"]')
+
+const form= fbody.querySelector('form')
+form.addEventListener('submit',function (event){
+    console.log(name.value)
+    event.preventDefault()
+})
